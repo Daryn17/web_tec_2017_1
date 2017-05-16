a4e2c3908f1f57d8139090c0704450ed00c2e34f
Daryn17 <darynsoto22@gmail.com>
2017-05-15 23:02:01 -0600

    heroku

--- WebScraping/WebScraping.py
+++ WebScraping/WebScraping.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import psycopg2
-import pprint
+import time
 
 #--------------------------------------------------------------------------
 
@@ -17,13 +17,9 @@ book = []
 
 #---------------------------------------------------------------------------
 
-def dropDB():
-    conection = psycopg2.connect("dbname='webScraping' user='postgres' host='localhost' password='123'")
-    objQuery = conection.cursor()
-    objQuery.execute('DELETE FROM public."bookTable";')
-    conection.commit()
-    objQuery.close()
-    conection.close()
+
+conection = psycopg2.connect(database="d8uqfr9b0s558k", user="almadmldqqdyxj", password="908fadd1cc02984877396b7cdf9593f83a484a43ba7563b852bfd80f3facd8f8", host="ec2-54-225-107-107.compute-1.amazonaws.com", port="5432")
+
 
 
 def conectionDB(book):
@@ -35,16 +31,20 @@ def conectionDB(book):
         nPages = book[3]
         nPages = nPages[:-6]
         nPages = int(nPages)
-        conection = psycopg2.connect("dbname='webScraping' user='postgres' host='localhost' password='123'")
+        conection = psycopg2.connect(database="d8uqfr9b0s558k", user="almadmldqqdyxj", password="908fadd1cc02984877396b7cdf9593f83a484a43ba7563b852bfd80f3facd8f8", host="ec2-54-225-107-107.compute-1.amazonaws.com", port="5432")
         objQuery = conection.cursor()
         objQuery.execute(
-                'INSERT INTO public."bookTable"("Author", "Name", "Price", "nPages", "Bookbinding", "Editorial", "Language", "ISBN") VALUES(%s, %s, %s, %s, %s, %s, %s, %s);',
-                (book[0], book[1], price, nPages, book[4], book[5], book[6], int(book[7])))
+                'INSERT INTO bookTable (Author, Name, Price, nPages, Bookbinding, Editorial, Language, ISBN) VALUES(%s, %s, %s, %s, %s, %s, %s, %s);',
+                (book[0], book[1], price, nPages, book[4], book[5], book[6], book[7]))
 
         objQuery.execute(
-            'SELECT "Author", "Name", "Price", "nPages", "Bookbinding", "Editorial", "Language", "ISBN" FROM public."bookTable";')
-        registro = objQuery.fetchall()
-        #pprint.pprint(registro)
+            'SELECT Author, Name, Price, nPages, Bookbinding, Editorial, Language, ISBN FROM bookTable;')
+        #registro = objQuery.fetchall()
+        #print.pprint(registro)
+        objQuery.execute(
+            'INSERT INTO auditoria(ID, Date, PageID, Registros, Estado, Errores) VALUES(%s, %s, %s, %s, %s, %s);',
+            (book[7], time.strftime("%d/%m/%y"),"www.casadellibro.com", count,"finalizado", "no"))
+
         conection.commit()
         objQuery.close()
         conection.close()
@@ -62,9 +62,37 @@ def cutText(cadena):
 
 #---------------------------------------------------------------------------
 
+def crearTablas():
+    cur = conection.cursor()
+
+    cur.execute('''CREATE TABLE Auditoria
+       (ID TEXT     NOT NULL,
+        PageID TEXT NOT NULL,
+        Date TEXT NOT NULL,
+        Errores TEXT NOT NULL,
+        Estado TEXT NOT NULL,
+        Registros INT NOT NULL);''')
+
+    cur.execute('''CREATE TABLE bookTable
+       (Author TEXT NOT NULL,
+        Name TEXT NOT NULL,
+        Price INT NOT NULL, 
+        nPages INT NOT NULL,
+        Bookbinding TEXT NOT NULL,
+        Editorial TEXT NOT NULL,
+        Language TEXT NOT NULL,
+        ISBN TEXT NOT NULL);''')
+    print("Table created successfully")
+
+    conection.commit()
+    conection.close()
+
+
+#---------------------------------------------------------
 
 
 
+crearTablas()
 
 source = urllib.request.urlopen('https://www.casadellibro.com/libros').read()
 soup = BeautifulSoup(source, 'html.parser')
